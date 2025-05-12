28f476d205941363098aa8caaae442e378f729ac
faniapriadi073 <faniapriadi073@gmail.com>
2025-05-12 21:18:46 +0800

    updete keranjang beli

---
+++ src/app.js
@@ -0,0 +1,84 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("products", () => ({
+    items: [
+      { id: 1, name: "Ivory Breeze", Img: "1.jpg", price: 129999 },
+      { id: 2, name: "Breeze", Img: "2.jpg", price: 129999 },
+    ],
+    showDetail(item) {
+      const modal = document.querySelector("#item-detail-modal");
+      // Gunakan Alpine.js untuk mengupdate state
+      Alpine.nextTick(() => {
+        modal._x_dataStack[0].selectedItem = { ...item };
+        modal.style.display = "flex";
+        feather.replace();
+      });
+    },
+  }));
+
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      // cek apakah ada barang yang sama di cart
+      const cartItem = this.items.find((item) => item.id === newItem.id);
+
+      // jika belum ada /cart kosong
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        // jika barang sudah ada, cek barang beda atau sama dgn yg ada di cart
+        this.items = this.items.map((item) => {
+          // jika barang berbeda
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            // jika barang sudah ada, tambah quantity dan totalnya
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+    },
+    remove(id) {
+      // ambil item yg diremove berdasarkan ID
+      const cartItem = this.items.find((item) => item.id === id);
+
+      // jika item lebih dari satu
+      if (cartItem.quantity > 1) {
+        // telusuri 1 1
+        this.items = this.items.map((item) => {
+          // jika bukan barang yg diklik
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        // jika barangnya sisa 1
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
+    },
+  });
+});
+
+// konversi ke rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
